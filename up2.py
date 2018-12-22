0ff670dc8a59f62dcd514d5a005a9f5504210388
Divyansh Jhunjhunwala <32570337+Divyansh03@users.noreply.github.com>
2018-12-22 22:29:00 +0530

    Keras implementation of LSTM with attention network(needs fixing)

---
+++ up2.py
@@ -0,0 +1,74 @@
+
+import numpy as np
+import keras
+import pickle
+from sklearn.utils import shuffle
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras import regularizers, constraints, initializers, activations
+from keras.models import Sequential
+from keras.layers import*
+def one_hot_encode(sequence, n_unique):
+	for i in range(len(sequence)):
+
+	    for j in range(1,11):
+		    np.append(sequence[i],[0])
+	return sequence
+#from keras.layers import Dense, Activation, Embedding, LSTM, Flatten, RepeatVector, Permute
+#from keras.layers import Bidirectional
+from attention_decoder import AttentionDecoder
+from AttentionWithContext import AttentionWithContext
+
+
+with open('objs.pkl') as f: 
+    X_train = pickle.load(f)
+y_train=[]
+for i in range(0,4):
+	for j in range(1,102):
+		y_train.append(i)
+for i in range(4,6):
+	for j in range(1,101):
+		y_train.append(i)
+
+X_train, y_train = shuffle(X_train, y_train)
+
+y_train = to_categorical(y_train, num_classes=None)
+
+
+
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+
+
+
+
+
+
+
+
+embedding_vector_length = 16
+model = Sequential() 
+model.add(Embedding(5000, embedding_vector_length, input_length=50))
+
+model.add(LSTM(150,  return_sequences=True))
+model.add(AttentionWithContext())
+model.add(Dense(6,activation='sigmoid'))
+
+
+
+model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+
+model.fit(X_train, y_train, validation_data=(X_test, y_test), nb_epoch=100, batch_size=64) 
+
+scores = model.evaluate(X_test, y_test, verbose=0) 
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+
+
+
+
+
+
